ff4edc6798b0168dc704904e639ac153d86d2545
aashish84 <aashishkr.thapa84@gmail.com>
2025-04-23 17:13:48 +0545

    featured destination get all

--- src/main/java/com/example/test/controller/FeaturedDestinationController.java
+++ src/main/java/com/example/test/controller/FeaturedDestinationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.print.attribute.standard.Media;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/featuredDestination")
@@ -27,6 +28,12 @@ public class FeaturedDestinationController {
         System.out.println(image.getContentType());
         FeaturedDestination result = featuredDestinationService.add(featuredDestination , image);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FeaturedDestination>> getFeaturedDestinations() {
+        List<FeaturedDestination> allData = featuredDestinationService.getAll();
+        return ResponseEntity.ok(allData);
     }
 
 }
--- src/main/java/com/example/test/service/FeaturedDestinationService.java
+++ src/main/java/com/example/test/service/FeaturedDestinationService.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class FeaturedDestinationService {
@@ -18,5 +20,9 @@ public class FeaturedDestinationService {
         String finalFileName = fileHelper.uploadFile(image);
         featuredDestination.setImage(finalFileName);
         return repo.save(featuredDestination);
+    }
+
+    public List<FeaturedDestination> getAll() {
+        return repo.findAll();
     }
 }
